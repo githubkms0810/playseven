dac13d3617d0fd1173730551a1b6a50824e44072
임수빈 <sbin910@gmail.com>
2018-04-04 18:21:32 +0900

    section3

--- application/modules/main/views/base/index.php
+++ application/modules/main/views/base/index.php
@@ -1,187 +1,183 @@
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#fullpage').fullpage({
+            css3: true,
+            anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
+            sectionsColor: ['#fab7b7', '#faa8a8', '#f09494', '#f08080'],
+            navigation: true,
+            navigationPosition: 'right',
+            navigationTooltips: ['Intro', 'Web', 'GolfPass', 'GolfBusiness'],
 
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $('#fullpage').fullpage({
-                css3: true,
-                anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
-                sectionsColor: ['#fab7b7', '#faa8a8', '#f09494', '#f08080'],
-                navigation: true,
-                navigationPosition: 'right',
-                navigationTooltips: ['Intro', 'Web', 'GolfPass', 'GolfBusiness'],
 
-
-                onLeave: function(index, nextIndex, direction) {
-                    if (index <= 3 && direction == 'down') {
-                        $('.section').eq(index - 1).removeClass('moveDown').addClass('moveUp');
-                    } else if (index <= 3 && direction == 'up') {
-                        $('.section').eq(index - 1).removeClass('moveUp').addClass('moveDown');
-                    }
+            onLeave: function(index, nextIndex, direction) {
+                if (index <= 3 && direction == 'down') {
+                    $('.section').eq(index - 1).removeClass('moveDown').addClass('moveUp');
+                } else if (index <= 3 && direction == 'up') {
+                    $('.section').eq(index - 1).removeClass('moveUp').addClass('moveDown');
                 }
-            });
+            }
         });
+    });
 
-    </script>
+</script>
 
 
-    <div id="fullpage">
-        <div class="section moveDown" id="section0">
-            <div class="footer">
-                <div class="footer_content">
-                    <a href="https://www.naver.com/" target="_blank"><img src="/public/images/main-footer.png"></a>
-                </div>
+<div id="fullpage">
+    <div class="section moveDown" id="section0">
+        <div class="footer">
+            <div class="footer_content">
+                <a href="https://www.naver.com/" target="_blank"><img src="/public/images/main-footer.png"></a>
             </div>
-            <div class="slide" id="slide1">
-                <div class="intro_container">
-                    <div class="intro">
-                        <div class="intro-title">
-                            <img src="/public/images/main-section-0_txt.png">
-                        </div>
-                        <div class="intro-btn">
-                            <a href="https://www.naver.com/" target="_blank">
-                                <div class="intro-btn_portfolio">
-                                </div>
-                            </a>
-                            <a href="#">
-                                <div class="intro-btn_movie">
-                                </div>
-                            </a>
-                        </div>
+        </div>
+        <div class="slide" id="slide1">
+            <div class="intro_container">
+                <div class="intro">
+                    <div class="intro-title">
+                        <img src="/public/images/main-section-0_txt.png">
+                    </div>
+                    <div class="intro-btn">
+                        <a href="https://www.naver.com/" target="_blank">
+                            <div class="intro-btn_portfolio">
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="intro-btn_movie">
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div>
-            <div class="slide" id="slide2">
-                <div class="intro_video">
-                    <video autoplay controls id="myvideo">
+        </div>
+        <div class="slide" id="slide2">
+            <div class="intro_video">
+                <video autoplay controls id="myvideo">
                         <source  src="//public/video/golfpass.mp4" type="video/mp4">
                          video 요소를 지원하지 않는 브라우저입니다. 동영상은 다운로드 후 볼 수 있습니다.
                     </video>
-                </div>
             </div>
         </div>
-        <div class="section moveDown" id="section1">
-            <div class="footer">
-                <div class="footer_content">
-                    <a href="https://www.naver.com/" target="_blank"><img src="/public/images/main-footer.png"></a>
-                </div>
+    </div>
+    <div class="section moveDown" id="section1">
+        <div class="footer">
+            <div class="footer_content">
+                <a href="https://www.naver.com/" target="_blank"><img src="/public/images/main-footer.png"></a>
             </div>
-            <div class="slide" id="slide1">
-                <div class="about_container">
-                    <div class="about">
-                        <div class="about-title">
-                            <img src="/public/images/main-section-1_txt.png">
-                        </div>
-                        <div class="about-content">
-                            <div style="background-image: url(/public/images/Monitor.png); background-repeat: no-repeat; background-position: center; height: 410px; margin: 0 auto;">
-                                <div id="sliderFrame">
-                                    <div id="slider">
-                                        <div>
-                                            <img src="/public/images/test1.png" />
-                                        </div>
-                                        <div>
-                                            <img src="/public/images/test2.png" />
-                                        </div>
-                                        <div>
-                                            <img src="/public/images/test3.png" />
-                                        </div>
-                                        <div>
-                                            <img src="/public/images/test4.png" />
-                                        </div>
-                                        <div>
-                                            <img src="/public/images/test5.png" />
-                                        </div>
-                                        <div>
-                                            <img src="/public/images/test6.png" />
-                                        </div>
+        </div>
+        <div class="slide" id="slide1">
+            <div class="about_container">
+                <div class="about">
+                    <div class="about-title">
+                        <img src="/public/images/main-section-1_txt.png">
+                    </div>
+                    <div class="about-content">
+                        <div style="background-image: url(/public/images/Monitor.png); background-repeat: no-repeat; background-position: center; height: 410px; margin: 0 auto;">
+                            <div id="sliderFrame">
+                                <div id="slider">
+                                    <div>
+                                        <img src="/public/images/test1.png" />
                                     </div>
-                                    <!--Custom navigation buttons on both sides-->
-                                    <div id="thumbs">
-                                        <div class="thumb"><img src="/public/images/navi1.png" /></div>
-                                        <div class="thumb"><img src="/public/images/navi2.png" /></div>
-                                        <div class="thumb"><img src="/public/images/navi3.png" /></div>
-                                        <div class="thumb"><img src="/public/images/navi4.png" /></div>
-                                        <div class="thumb"><img src="/public/images/navi5.png" /></div>
-                                        <div class="thumb"><img src="/public/images/navi6.png" /></div>
+                                    <div>
+                                        <img src="/public/images/test2.png" />
+                                    </div>
+                                    <div>
+                                        <img src="/public/images/test3.png" />
+                                    </div>
+                                    <div>
+                                        <img src="/public/images/test4.png" />
+                                    </div>
+                                    <div>
+                                        <img src="/public/images/test5.png" />
+                                    </div>
+                                    <div>
+                                        <img src="/public/images/test6.png" />
                                     </div>
                                 </div>
+                                <!--Custom navigation buttons on both sides-->
+                                <div id="thumbs">
+                                    <div class="thumb"><img src="/public/images/navi1.png" /></div>
+                                    <div class="thumb"><img src="/public/images/navi2.png" /></div>
+                                    <div class="thumb"><img src="/public/images/navi3.png" /></div>
+                                    <div class="thumb"><img src="/public/images/navi4.png" /></div>
+                                    <div class="thumb"><img src="/public/images/navi5.png" /></div>
+                                    <div class="thumb"><img src="/public/images/navi6.png" /></div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="section moveDown" id="section2">
-            <div class="footer">
-                <div class="footer_content">
-                    <a href="https://www.naver.com/" target="_blank"><img src="/public/images/main-footer.png"></a>
-                </div>
-            </div>
-            <div class="slide" id="slide1">
-                <div class="features_container" id="golfpass1">
-                    <div class="features">
-                        <div class="features-title">
-                            <img src="/public/images/main-section-2_txt.png">
-                        </div>
-                        <div style="background-image: url(/public/images/searchbox.png); background-repeat: no-repeat; margin-top: 30px;">
-                            <div class="features-content" onclick="window.open('about:blank').location.href='http://golfpass.net'" style="cursor: pointer; width: 356px; height: 50px;">
-                                골프패스
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="slide" id="slide2">
-                <div class="features_container">
-                    <div class="features">
-                        <div class="features-title">
-                            GolfPass<br>골프패스 <br>
-                        </div>
-                        <div class="features-content">
-                            인포그ㅡ래픽 영상 삽입 자막 일본어 필요
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="section moveDown" id="section3">
-            <div class="footer">
-                <div class="footer_content">
-                    <a href="https://www.naver.com/" target="_blank"><img src="/public/images/main-footer.png"></a>
-                </div>
-            </div>
-            <div class="slide" id="slide1">
-                <div class="portfolio_container">
-                    <div class="portfolio">
-                        <div class="portfolio-title">
-                            GolfBusiness<br> 골프비즈니스
-                            <br>
-                        </div>
-                        <div class="portfolio-content">
-                            해외 골프장 업계 현황 데이터 및 M&A물건 정보 및 컨섩팅
-                        </div>
-                        <div style="background-image: url(/public/images/submit.png); background-repeat: no-repeat; width: 437px; height: 100px; margin-top: 70px;">
-                            <input class="telinput" type="text" placeholder="전화번호입력">
-                            <input class="telsubmit" type="button">
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="section moveDown" id="section2">
+        <div class="footer">
+            <div class="footer_content">
+                <a href="https://www.naver.com/" target="_blank"><img src="/public/images/main-footer.png"></a>
+            </div>
+        </div>
+        <div class="slide" id="slide1">
+            <div class="features_container" id="golfpass1">
+                <div class="features">
+                    <div class="features-title">
+                        <img src="/public/images/main-section-2_txt.png">
+                    </div>
+                    <div style="background-image: url(/public/images/searchbox.png); background-repeat: no-repeat; margin-top: 30px;">
+                        <div class="features-content" onclick="window.open('about:blank').location.href='http://golfpass.net'" style="cursor: pointer; width: 356px; height: 50px;">
+                            골프패스
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="slide" id="slide2">
+            <div class="features_container">
+                <div class="features">
+                    <div class="features-title">
+                        GolfPass<br>골프패스 <br>
+                    </div>
+                    <div class="features-content">
+                        인포그ㅡ래픽 영상 삽입 자막 일본어 필요
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section moveDown" id="section3">
+        <div class="footer">
+            <div class="footer_content">
+                <a href="https://www.naver.com/" target="_blank"><img src="/public/images/main-footer.png"></a>
+            </div>
+        </div>
+        <div class="slide" id="slide1">
+            <div class="portfolio_container">
+                <div class="portfolio">
+                    <div class="portfolio-title">
+                        <img src="/public/images/main-section-3_txt.png">
+                    </div>
 
-    <script src="js/slider.js"></script>
-    <script type="text/javascript">
-        //The following script is for the group 2 navigation buttons.
-        function switchAutoAdvance() {
-            imageSlider.switchAuto();
-            switchPlayPauseClass();
-        }
+                    <div style="background-image: url(/public/images/submit.png); background-repeat: no-repeat; width: 437px; height: 100px; margin-top: 30px;">
+                        <input class="telinput" type="text" placeholder="전화번호입력">
+                        <input class="telsubmit" type="button">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-        function switchPlayPauseClass() {
-            var auto = document.getElementById('auto');
-            var isAutoPlay = imageSlider.getAuto();
-            auto.className = isAutoPlay ? "group2-Pause" : "group2-Play";
-            auto.title = isAutoPlay ? "Pause" : "Play";
-        }
+<script src="js/slider.js"></script>
+<script type="text/javascript">
+    //The following script is for the group 2 navigation buttons.
+    function switchAutoAdvance() {
+        imageSlider.switchAuto();
         switchPlayPauseClass();
+    }
 
-    </script>
+    function switchPlayPauseClass() {
+        var auto = document.getElementById('auto');
+        var isAutoPlay = imageSlider.getAuto();
+        auto.className = isAutoPlay ? "group2-Pause" : "group2-Play";
+        auto.title = isAutoPlay ? "Pause" : "Play";
+    }
+    switchPlayPauseClass();
+
+</script>
