6970d4ff02a1f663a085a71b9a8b2e77fbbd7678
임수빈 <sbin910@gmail.com>
2018-04-06 17:31:58 +0900

    marketing

--- application/modules/playseven_marketing/views/base/index_marketing.php
+++ application/modules/playseven_marketing/views/base/index_marketing.php
@@ -1,11 +1,21 @@
 <link rel="stylesheet" type="text/css" href="/public/css/style_marketing.css" />
-<div class="section fp-auto-height" id="section0" style="background-color: aliceblue;">
+
+<div class="section fp-auto-height" id="section0" style="background-color: #fff;">
     <div class="marketings">
         <div class="marketings_title">
-            Golf Passes Leading the New Golf Culture
+            <div class="company-about-wrap">
+            <div class="w1100_wrap pt-120">
+                <p class="company-about-tit">COMPANY</p>
+                <h3>새로운 골프문화를 선도하는 골프패스</h3>
+                <div class="company-about-txt">
+                    <div class=" pt-50">
+                        안녕하십니까. 골프패스입니다.<br> 골프는 많은 현대인들의 친구이자 위로입니다. 더 가까운 친구, 더 즐겁고<br> 따뜻한 위로가 되도록 하겠습니다.직원들, 회원들, 골퍼들, 골프장 모두의 새로운<br> 행복을 위하여 항상 노력하는 골프패스가 되겠습니다.<br>
+                    </div>
+                    <div class="hr_line5 pt-60"></div>
+                </div>
+
+            </div>
         </div>
-        <div class="marketing_content">
-            Hello. Golf passes. Golf is a friend and back of many modern people. I will be a closer friend, a more pleasant and warmer comfort. I will always strive for the happiness of our employees, members, golfers and golfers .
         </div>
     </div>
 </div>
